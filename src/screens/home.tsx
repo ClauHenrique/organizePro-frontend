6458c87c4a9cf6ec056b7f3cb2c1ce8e53ae9ef4
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2024-06-16 11:25:44 -0300

    add bootstrap

---
+++ src/screens/home.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header/header";
+
+export default function Home() {
+
+
+  return (
+    <>
+     <Header userName={"claudio Henrique"} />
+     <h1>Tarefas cadastradas</h1>
+    </>
+  )
+}
+
