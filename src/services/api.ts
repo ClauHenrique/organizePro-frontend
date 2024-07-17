fb8cbef32cf87d8cfdb2821d35bb91e56c0763e8
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2024-07-17 16:15:31 -0300

    add url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// import.meta.env.VITE_API_URL
+
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: 'https://api-organize-pro.azurewebsites.net',
     timeout: 1000,
     headers: {'X-Custom-Header': 'foobar'}
   });
